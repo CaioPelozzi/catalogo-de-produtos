c517fa316a08c7d4d16dc3a181e451f88030949e
Caio Pelozzi <caiopelozzi18@gmail.com>
2025-07-25 22:43:40 -0300

    Criei o MAP, proximo passo programa principal

---
+++ src/model/utils/ProdutosParaMapa.java
@@ -0,0 +1,21 @@
+package model.utils;
+
+import model.entities.Produto;
+import model.enums.Categoria;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ProdutosParaMapa {
+
+    public static Map<Categoria, List<Produto>> listaParaMapa(List<Produto> produtos) {
+        return produtos.stream().collect(Collectors.groupingBy(Produto::getCategoria));
+    }
+
+    /*public static String imprimirMapa(Map<Categoria, List<Produto>> mapa) {
+        return mapa.forEach(
+                p ->
+        );
+    }*/
+}
